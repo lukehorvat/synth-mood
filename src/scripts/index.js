1c3fefa47b87037bafa19401f4fb2c60c51256ed
Luke Horvat <lukehorvat@gmail.com>
2020-01-19 03:39:49 +0100

    Remove animated description

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -22,7 +22,7 @@ const fontCache = new FontCache("fonts");
 const modelCache = new ModelCache("models");
 const soundCache = new SoundCache("sounds");
 
-let renderer, camera, scene, description, title, gridTop, gridBottom, spotlight, models, sounds;
+let renderer, camera, scene, title, gridTop, gridBottom, spotlight, models, sounds;
 
 init().then(render);
 
@@ -61,14 +61,6 @@ function init() {
     scene = new THREE.Scene();
 
     let font = fontCache.get("Righteous_Regular.json");
-    description = new THREE.Mesh(new THREE.TextGeometry("SLEEP, STUDY, AND RELAX WITH THE SOUND OF SYNTHESIZERS!", { font, size: 35, height: 1 }));
-    description.material = Object.assign(material.clone(), { transparent: true });
-    description.geometry.computeBoundingBox(); // Compute bounding box so that text can be centered.
-    description.position.x = description.geometry.boundingBox.min.x - description.geometry.boundingBox.max.x / 2;
-    description.position.x -= 2; // FIXME: Text doesn't center properly; a bug in FontLoader?
-    description.position.y = description.geometry.boundingBox.min.y - description.geometry.boundingBox.max.y / 2;
-    description.position.z = camera.position.z + 250;
-    scene.add(description);
 
     title = new THREE.Mesh(new THREE.TextGeometry("SYNTH MOOD", { font, size: 110, height: 1 }));
     title.material = material;
@@ -76,7 +68,7 @@ function init() {
     title.position.x = title.geometry.boundingBox.min.x - title.geometry.boundingBox.max.x / 2;
     title.position.x -= 6; // FIXME: Text doesn't center properly; a bug in FontLoader?
     title.position.y = title.geometry.boundingBox.min.y - title.geometry.boundingBox.max.y / 2;
-    title.position.z = camera.position.z + 1250;
+    title.position.z = camera.position.z + 250;
     scene.add(title);
 
     gridTop = new THREE.GridHelper(gridSize, gridDivisions);
@@ -118,17 +110,9 @@ function render() {
   gridTop.position.z += gridTop.position.z < gridSize / gridDivisions ? 1 : -gridTop.position.z;
   gridBottom.position.z += gridBottom.position.z < gridSize / gridDivisions ? 1 : -gridBottom.position.z;
 
-  if (description.position.z > 0) {
-    // Move description away from the camera until it reaches its resting position.
-    description.position.z = Math.max(description.position.z - 5, 0);
-  } else if (title.position.z > 0) {
+  if (title.position.z > 0) {
     // Move title away from the camera until it reaches its resting position.
     title.position.z = Math.max(title.position.z - 5, 0);
-
-    // Fade-out description once title enters the picture.
-    if (title.position.z < camera.position.z) {
-      description.material.opacity -= 0.008;
-    }
   } else {
     // Move models closer to the camera.
     // Destroy models once they have travelled past the camera.
